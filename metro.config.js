8c5d95c4c741f6b68567cd459b30e578db2da59e
denn18 <114248881+denn18@users.noreply.github.com>
2026-06-15 13:34:38 +0200

    Fix Expo Metro startup

--- metro.config.js
+++ metro.config.js
@@ -7,13 +7,20 @@ const config = getDefaultConfig(projectRoot);
 
 config.watchFolders = [projectRoot];
 config.resolver.disableHierarchicalLookup = true;
-config.resolver.nodeModulesPaths = [path.join(projectRoot, 'node_modules')];
+config.resolver.nodeModulesPaths = [
+  path.join(projectRoot, 'node_modules'),
+  path.join(projectRoot, 'node_modules/react-native/node_modules'),
+];
 config.resolver.blockList = exclusionList([
-  /(^|[/\\])\.git([/\\].*)?$/,
-  /(^|[/\\])\.expo([/\\].*)?$/,
-  /(^|[/\\])dist([/\\].*)?$/,
-  /(^|[/\\])build([/\\].*)?$/,
-  /(^|[/\\])coverage([/\\].*)?$/,
+  new RegExp(`${escapePath(projectRoot)}[/\\\\]\\.git([/\\\\].*)?$`),
+  new RegExp(`${escapePath(projectRoot)}[/\\\\]\\.expo([/\\\\].*)?$`),
+  new RegExp(`${escapePath(projectRoot)}[/\\\\]dist([/\\\\].*)?$`),
+  new RegExp(`${escapePath(projectRoot)}[/\\\\]build([/\\\\].*)?$`),
+  new RegExp(`${escapePath(projectRoot)}[/\\\\]coverage([/\\\\].*)?$`),
 ]);
+
+function escapePath(filePath) {
+  return filePath.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+}
 
 module.exports = config;
